ae9cb411330d259b9d4537369794d8c3957cb253
王军杰 <iot_wangjunjie@126.com>
2017-07-22 10:00:31 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * This file write with C language, It will call middle layer,
+ * The middle layer will call C++ function.
+ * Auth: jacky-wangjj Email: iot_wangjunjie@126.com
+ * Data: 1017/07/22
+ */
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
+ #include "include.h"
+ 
+ void main()
+ {
+ 
+ }
